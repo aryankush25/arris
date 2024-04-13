58d5bb796a53b23b8ff0bff7a48859032fbf4323
Aryan Agarwal <aryankush025@gmail.com>
2024-04-13 17:28:36 +0530

    Refactor page routing and add registration page

--- arris/arris.py
+++ arris/arris.py
@@ -6,9 +6,11 @@ import reflex as rx
 
 from arris.styles import BACKGROUND_COLOR, FONT_FAMILY, THEME, STYLESHEETS
 
-from arris.pages.tools import tools
-from arris.pages.team import team
 from arris.pages.index import index
+from arris.pages.register import register
+
+# from arris.pages.tools import tools
+# from arris.pages.team import team
 
 # Create app instance and add index page.
 app = rx.App(
@@ -17,5 +19,6 @@ app = rx.App(
 )
 
 app.add_page(index, route="/")
-app.add_page(tools, route="/tools")
-app.add_page(team, route="/team")
+app.add_page(register, route="/register")
+# app.add_page(tools, route="/tools")
+# app.add_page(team, route="/team")
