cdac79f70ff81c17ecb1e414232ce2487cbd469b
Aryan Agarwal <aryankush025@gmail.com>
2024-04-14 00:52:04 +0530

    Add login page and form handling in login.py

--- arris/arris.py
+++ arris/arris.py
@@ -8,6 +8,7 @@ from arris.styles import BACKGROUND_COLOR, FONT_FAMILY, THEME, STYLESHEETS
 
 from arris.pages.index import index
 from arris.pages.register import register
+from arris.pages.login import login
 
 # from arris.pages.tools import tools
 # from arris.pages.team import team
@@ -19,6 +20,7 @@ app = rx.App(
 )
 
 app.add_page(index, route="/")
+app.add_page(login, route="/login")
 app.add_page(register, route="/register")
 # app.add_page(tools, route="/tools")
 # app.add_page(team, route="/team")
---
+++ arris/pages/login.py
@@ -0,0 +1,83 @@
+import reflex as rx
+from passlib.hash import pbkdf2_sha256
+
+from arris.utils import ClientStorageState
+import jwt
+
+
+from arris.schemas.user import get_user
+
+
+class LoginRadixFormSubmissionState(rx.State):
+    form_data: dict
+
+    def handle_submit(self, form_data: dict):
+        """Handle the form submit."""
+        print(form_data)
+
+        user = get_user(
+            email=form_data["email"],
+        )
+
+        user.password
+
+        # TODO: Use .env file for secret
+
+        if user and pbkdf2_sha256.verify(form_data["password"], user.password):
+            encoded = jwt.encode(
+                {"some": form_data["email"]}, "secret", algorithm="HS256"
+            )
+            yield [rx.redirect("/"), ClientStorageState.set_custom_cookie(encoded)]
+
+
+def login() -> rx.Component:
+
+    return rx.box(
+        rx.form.root(
+            rx.form.field(
+                rx.flex(
+                    rx.form.label("Email"),
+                    rx.form.control(
+                        rx.input.input(
+                            placeholder="Email Address",
+                            # type attribute is required for "typeMismatch" validation
+                            type="email",
+                        ),
+                        as_child=True,
+                    ),
+                    rx.form.message(
+                        "Please enter a valid email",
+                        match="typeMismatch",
+                    ),
+                    direction="column",
+                    spacing="2",
+                ),
+                name="email",
+            ),
+            rx.form.field(
+                rx.flex(
+                    rx.form.label("Password"),
+                    rx.form.control(
+                        rx.input.input(
+                            placeholder="Enter your password",
+                            type="password",
+                            min_length=8,
+                        ),
+                        as_child=True,
+                    ),
+                    rx.form.message(
+                        "Please enter a password length >= 8",
+                        match="tooShort",
+                    ),
+                    direction="column",
+                    spacing="2",
+                ),
+                name="password",
+            ),
+            rx.form.submit(
+                rx.button("Submit"),
+                as_child=True,
+            ),
+            on_submit=LoginRadixFormSubmissionState.handle_submit,
+        ),
+    )
