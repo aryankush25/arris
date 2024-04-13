760c5c2f339fd44b54f4260b72706051d7f15044
Aryan Agarwal <aryankush025@gmail.com>
2024-04-13 15:48:21 +0530

    Refactor user table name in database migration script and model

--- models/user.py
+++ models/user.py
@@ -1,9 +1,10 @@
 import reflex as rx
-
 from sqlmodel import Field
 
 
 class User(rx.Model, table=True):
+    __tablename__ = "users"
+
     id: int = Field(default=None, primary_key=True)
     username: str
     full_name: str
