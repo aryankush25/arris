e719f8f9358ca03dd4f64c62ead4226d080cb173
Aryan Agarwal <aryankush025@gmail.com>
2024-04-14 03:30:22 +0530

    Refactor utils.py to add email-related functions and JWT token generation

--- arris/utils.py
+++ arris/utils.py
@@ -1,4 +1,6 @@
 import reflex as rx
+import jwt
+from rxconfig import config
 
 
 class ClientStorageState(rx.State):
@@ -7,6 +9,25 @@ class ClientStorageState(rx.State):
         path="/",
         name="access_token",
     )
+
+    def get_email(self):
+        if not self.custom_cookie:
+            return None
+
+        payload = jwt.decode(
+            self.custom_cookie,
+            config.jwt_secret,
+            algorithms=["HS256"],
+        )
+
+        return payload["email"]
+
+    def generate_token(self, email: str):
+        return jwt.encode(
+            {"email": email},
+            config.jwt_secret,
+            algorithm="HS256",
+        )
 
     def logout(self):
         yield [rx.remove_cookie("access_token"), rx.redirect("/login")]
