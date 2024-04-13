e0f16dcafc3fe19ea1ab51eb495c2d5ca63cb8e9
Ishita <ishita@gluelabs.com>
2024-04-14 00:29:54 +0530

    review section done

---
+++ arris/component/reviewsection.py
@@ -0,0 +1,229 @@
+import reflex as rx
+
+def reviewsection() -> rx.Component:
+    return rx.box(
+        rx.text(
+            "Our customers love us",
+            class_name="text-[##323131] font-medium text-[40px]"
+        ),
+        rx.box(
+            rx.box(
+                rx.box(
+                    rx.box(
+                        rx.box(
+                            class_name="rounded-full w-7 h-7 bg-gray-500"
+                        ),
+                        rx.text(
+                            "Cédric van Ravesteijn",
+                            class_name="font-semibold"
+                        ),
+                        class_name="flex items-cemter gap-2"
+                    ),
+                    rx.box(
+                        "Framer has helped me to scale my agency by being able to make websites in a very efficient and creative way.",
+                        class_name="text-[##7A7A7A]"
+                    ),
+                    class_name="flex flex-col gap-2.5 max-w-[255px] w-full border bg-white rounded-3xl p-5"
+                ),
+                rx.box(
+                    rx.box(
+                        rx.box(
+                            class_name="rounded-full w-7 h-7 bg-gray-500"
+                        ),
+                        rx.text(
+                            "Melody O.",
+                            class_name="font-semibold"
+                        ),
+                        class_name="flex items-cemter gap-2"
+                    ),
+                    rx.box(
+                        "As a seasoned designer always on the lookout for innovative tools, Framer.com instantly grabbed my attention. This powerful web design platform is more than just a design tool - it's a complete ecosystem that revolutionizes the way websites are created and published.",
+                        class_name="text-[##7A7A7A]"
+                    ),
+                    class_name="flex flex-col gap-2.5 max-w-[255px] w-full border bg-white rounded-3xl p-5"
+                ),
+                rx.box(
+                    rx.box(
+                        rx.box(
+                            class_name="rounded-full w-7 h-7 bg-gray-500"
+                        ),
+                        rx.text(
+                            "Thaer Swailem",
+                            class_name="font-semibold"
+                        ),
+                        class_name="flex items-cemter gap-2"
+                    ),
+                    rx.box(
+                        "Framer has helped me to design great websites with half the effort.",
+                        class_name="text-[##7A7A7A]"
+                    ),
+                    class_name="flex flex-col gap-2.5 max-w-[255px] w-full border bg-white rounded-3xl p-5"
+                ),
+                class_name="flex flex-col gap-5"
+            ),
+            rx.box(
+                rx.box(
+                    rx.box(
+                        rx.box(
+                            class_name="rounded-full w-7 h-7 bg-gray-500"
+                        ),
+                        rx.text(
+                            "Lenderson M.",
+                            class_name="font-semibold"
+                        ),
+                        class_name="flex items-cemter gap-2"
+                    ),
+                    rx.box(
+                        "Framer offers an unparalleled design experience with its seamless integration of design and development. It's user-friendly, efficient, and promotes creativity, making it an essential tool for anyone looking to quickly bring their ideas to life.",
+                        class_name="text-[##7A7A7A]"
+                    ),
+                    class_name="flex flex-col gap-2.5 max-w-[255px] w-full border bg-white rounded-3xl p-5"
+                ),
+                rx.box(
+                    rx.box(
+                        rx.box(
+                            class_name="rounded-full w-7 h-7 bg-gray-500"
+                        ),
+                        rx.text(
+                            "Haris Sulaiman",
+                            class_name="font-semibold"
+                        ),
+                        class_name="flex items-cemter gap-2"
+                    ),
+                    rx.box(
+                        "Framer has helped me improve the SEO of @cloodot by 250% since July when we first launched with Framer.",
+                        class_name="text-[##7A7A7A]"
+                    ),
+                    class_name="flex flex-col gap-2.5 max-w-[255px] w-full border bg-white rounded-3xl p-5"
+                ),
+                rx.box(
+                    rx.box(
+                        rx.box(
+                            class_name="rounded-full w-7 h-7 bg-gray-500"
+                        ),
+                        rx.text(
+                            "Rico Trevisan",
+                            class_name="font-semibold"
+                        ),
+                        class_name="flex items-cemter gap-2"
+                    ),
+                    rx.box(
+                        "Absolutely excellent. A good prompt will generate a whole page: sections, colors, fonts, copy, graphics, everything. Wow!",
+                        class_name="text-[##7A7A7A]"
+                    ),
+                    class_name="flex flex-col gap-2.5 max-w-[255px] w-full border bg-white rounded-3xl p-5"
+                ),
+                class_name="flex flex-col gap-5"
+            ),
+            rx.box(
+                rx.box(
+                    rx.box(
+                        rx.box(
+                            class_name="rounded-full w-7 h-7 bg-gray-500"
+                        ),
+                        rx.text(
+                            "Cristian Nobile",
+                            class_name="font-semibold"
+                        ),
+                        class_name="flex items-cemter gap-2"
+                    ),
+                    rx.box(
+                        "Framer has helped me to create much more functional no-code sites for my customers due to the increased speed and freedom to create the UI.",
+                        class_name="text-[##7A7A7A]"
+                    ),
+                    class_name="flex flex-col gap-2.5 max-w-[255px] w-full border bg-white rounded-3xl p-5"
+                ),
+                rx.box(
+                    rx.box(
+                        rx.box(
+                            class_name="rounded-full w-7 h-7 bg-gray-500"
+                        ),
+                        rx.text(
+                            "Madhusudhan",
+                            class_name="font-semibold"
+                        ),
+                        class_name="flex items-cemter gap-2"
+                    ),
+                    rx.box(
+                        "I used to be a Webflow user. I relied on Webflow for all my freelance projects. However, when Framer was launched, I was amazed by its smooth performance. Typically, it took me around 5 days to complete websites, but with Framer, I did it in just 2 days. @Framer is impressive! 🔥",
+                        class_name="text-[##7A7A7A]"
+                    ),
+                    class_name="flex flex-col gap-2.5 max-w-[255px] w-full border bg-white rounded-3xl p-5"
+                ),
+                rx.box(
+                    rx.box(
+                        rx.box(
+                            class_name="rounded-full w-7 h-7 bg-gray-500"
+                        ),
+                        rx.text(
+                            "Mitchell B.",
+                            class_name="font-semibold"
+                        ),
+                        class_name="flex items-cemter gap-2"
+                    ),
+                    rx.box(
+                        "The incredibly control you have over everything in a drag and drop interface makes it fun to create for the web again. It's also incredibly quick to go from ",
+                        class_name="text-[##7A7A7A]"
+                    ),
+                    class_name="flex flex-col gap-2.5 max-w-[255px] w-full border bg-white rounded-3xl p-5"
+                ),
+                class_name="flex flex-col gap-5"
+            ),
+            rx.box(
+                rx.box(
+                    rx.box(
+                        rx.box(
+                            class_name="rounded-full w-7 h-7 bg-gray-500"
+                        ),
+                        rx.text(
+                            "Andreas Blomqvist",
+                            class_name="font-semibold"
+                        ),
+                        class_name="flex items-cemter gap-2"
+                    ),
+                    rx.box(
+                        "Framer has helped me turn ideas into high-quality websites with 10x the speed",
+                        class_name="text-[##7A7A7A]"
+                    ),
+                    class_name="flex flex-col gap-2.5 max-w-[255px] w-full border bg-white rounded-3xl p-5"
+                ),
+                rx.box(
+                    rx.box(
+                        rx.box(
+                            class_name="rounded-full w-7 h-7 bg-gray-500"
+                        ),
+                        rx.text(
+                            "Yã izidoro",
+                            class_name="font-semibold"
+                        ),
+                        class_name="flex items-cemter gap-2"
+                    ),
+                    rx.box(
+                        "Framer help me to do websites in 3 days, without any code 😅",
+                        class_name="text-[##7A7A7A]"
+                    ),
+                    class_name="flex flex-col gap-2.5 max-w-[255px] w-full border bg-white rounded-3xl p-5"
+                ),
+                rx.box(
+                    rx.box(
+                        rx.box(
+                            class_name="rounded-full w-7 h-7 bg-gray-500"
+                        ),
+                        rx.text(
+                            "Timothy Upai Lindris",
+                            class_name="font-semibold"
+                        ),
+                        class_name="flex items-cemter gap-2"
+                    ),
+                    rx.box(
+                        "My experience with Framer has been nothing short of exceptional. It has elevated my design process, empowered collaboration, and helped me deliver outstanding results. If you're a designer looking to level up your skills and create stunning interactive designs, I highly recommend giving Framer a try.",
+                        class_name="text-[##7A7A7A]"
+                    ),
+                    class_name="flex flex-col gap-2.5 max-w-[255px] w-full border bg-white rounded-3xl p-5"
+                ),
+                class_name="flex flex-col gap-5"
+            ),
+            class_name="flex gap-5"
+        ),
+        class_name="max-w-7xl mx-auto w-full flex flex-col gap-5 items-center mt-28"
+    )
