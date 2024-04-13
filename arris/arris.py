abf9a6306e10b3c94b80a68d3e2332f4a05b06b0
Aryan Agarwal <aryankush025@gmail.com>
2024-04-14 01:50:38 +0530

    Refactor health function and remove commented code in arris.py

--- arris/arris.py
+++ arris/arris.py
@@ -9,13 +9,6 @@ from arris.pages.register import register
 from arris.services.shopify import shopifyOAuthCallback
 from arris.pages.login import login
 
-# from arris.pages.tools import tools
-# from arris.pages.team import team
-
-
-def health():
-    yield [rx.redirect(f"https://google.com")]
-
 
 # Create app instance and add index page.
 app = rx.App(
@@ -27,7 +20,3 @@ app.add_page(index, route="/")
 app.add_page(login, route="/login")
 app.add_page(register, route="/register")
 app.api.add_api_route("/oauth/callback/", shopifyOAuthCallback)
-app.api.add_api_route("/health", health, methods=["GET"])
-# app.api.add_api_route("/install-shopify", install_app("xg-dev", "ravi149185@gmail.com"))
-# app.add_page(tools, route="/tools")
-# app.add_page(team, route="/team")
