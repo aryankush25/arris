20bfa3304ce680f57f4a0b3f249fbdf19c7e0efc
Ishita <ishita@gluelabs.com>
2024-04-13 21:34:18 +0530

    feature section completed

--- arris/component/featuresection.py
+++ arris/component/featuresection.py
@@ -2,7 +2,10 @@ import reflex as rx
 
 def featuresection() -> rx.Component:
     return rx.box(
-    rx.box(
+        rx.box(
+            border_radius="50% 50% 50% 50% / 0% 0% 100% 100%"
+        ),
+        rx.box(
         rx.box(
             rx.text(
                 "Features",
@@ -31,47 +34,47 @@ def featuresection() -> rx.Component:
                         src="/screen.png",
                         class_name="mx-auto w-64 h-40"
                     ),
-                class_name="p-6 flex gap-4 flex-col rounded-lg bg-white max-w-sm w-full"
+                class_name="p-6 flex gap-4 flex-col rounded-lg bg-white max-w-[360px] w-full"
             ),
             rx.box(
                 rx.box(
                     rx.text(
-                    "AI Generated Webpages",
+                    "Smart Prototyping using AI",
                     class_name="text-black font-bold text-3xl"
                 ),
                 rx.text(
-                    "Using AI to create websites offers speed and flexibility to any new design project. Build an AI generated website in real-time by writing a detailed prompt for the content you want to center the site on.",
+                    "Quickly transition from a simple idea to a fully functional prototype using AI for web development, which lets you go from an idea to a fully functioning prototype in minutes rather than hours.",
                     class_name="text-[#33373A99]"
                 ),
                 class_name="flex flex-col gap-1.5"
                 ),
                 rx.image(
-                        src="/screen.png",
+                        src="/phone.png",
                         class_name="mx-auto w-64 h-40"
                     ),
-                class_name="p-6 flex gap-4 flex-col rounded-lg bg-white max-w-sm w-full"
+                class_name="p-6 flex gap-4 flex-col rounded-lg bg-white max-w-[360px] w-full"
             ),
             rx.box(
                 rx.box(
                     rx.text(
-                    "AI Generated Webpages",
+                    "Drag and drop editing",
                     class_name="text-black font-bold text-3xl"
                 ),
                 rx.text(
-                    "Using AI to create websites offers speed and flexibility to any new design project. Build an AI generated website in real-time by writing a detailed prompt for the content you want to center the site on.",
+                    "Drag and Drop to create responsive pages and sections. Add text, buttons and pull product images straight from Shopify.",
                     class_name="text-[#33373A99]"
                 ),
                 class_name="flex flex-col gap-1.5"
                 ),
                 rx.image(
-                        src="/screen.png",
+                        src="/dragdrop.png",
                         class_name="mx-auto w-64 h-40"
                     ),
-                class_name="p-6 flex gap-4 flex-col rounded-lg bg-white max-w-sm w-full"
+                class_name="p-6 flex gap-4 flex-col rounded-lg bg-white max-w-[360px] w-full"
             ),
             class_name="flex w-full justify-between"
         ),
         class_name="flex flex-col items-center mx-auto max-w-7xl w-full gap-12"
     ),
-    class_name="bg-[#5B9BF3] w-full mx-auto"
+    class_name="bg-[#5B9BF3] w-full mx-auto py-32",
 )
