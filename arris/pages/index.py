dbf3ddd19423bcac13ecb26fc7fb08198539989c
Aryan Agarwal <aryankush025@gmail.com>
2024-04-14 04:02:00 +0530

    Update button functionality and add welcome message in index.py

--- arris/pages/index.py
+++ arris/pages/index.py
@@ -1,25 +1,12 @@
 """The main index page."""
 
 import reflex as rx
-from arris.utils import ClientStorageState
 
 
 def index() -> rx.Component:
 
     return rx.box(
-        rx.button(
-            "Logout",
-            color_scheme="ruby",
-            on_click=ClientStorageState.logout,
-        ),
-        rx.button(
-            "Decrement",
-            color_scheme="ruby",
-        ),
-        rx.box(
-            margin_top="calc(50px + 2em)",
-            padding="2em",
-        ),
+        rx.text("Welcome to Arris"),
         padding_left="250px",
         padding_top="100px",
     )
