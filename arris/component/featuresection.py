5407d69f6ad87047a7d7ac48422e07540f2d3277
Ishita <ishita@gluelabs.com>
2024-04-13 20:19:39 +0530

    feature section box

--- arris/component/featuresection.py
+++ arris/component/featuresection.py
@@ -6,17 +6,18 @@ def featuresection() -> rx.Component:
         rx.box(
             rx.text(
                 "Features",
-                class_name="text-white"
+                class_name="text-white font-bold text-[32px]"
             ),
-            rx.box(
+            rx.text(
                 "Strategic services drive digital success with tailored, comprehensive approaches.",
-                class_name="text-white"
+                class_name="text-white font-medium text-xl backdrop-opacity-80"
             ),
-            class_name="flex flex-col border items-center gap-5"
+            class_name="flex flex-col items-center gap-5"
         ),
         rx.box(
             rx.box(
-                rx.text(
+                rx.box(
+                    rx.text(
                     "AI Generated Webpages",
                     class_name="text-black font-bold text-3xl"
                 ),
@@ -24,11 +25,50 @@ def featuresection() -> rx.Component:
                     "Using AI to create websites offers speed and flexibility to any new design project. Build an AI generated website in real-time by writing a detailed prompt for the content you want to center the site on.",
                     class_name="text-[#33373A99]"
                 ),
-                rx.box(),
-                class_name="p-6 flex flex-col rounded-lg bg-white max-w-sm w-full"
+                class_name="flex flex-col gap-1.5"
+                ),
+                rx.image(
+                        src="/screen.png",
+                        class_name="mx-auto w-64 h-40"
+                    ),
+                class_name="p-6 flex gap-4 flex-col rounded-lg bg-white max-w-sm w-full"
             ),
-            rx.box(),
-            rx.box(),
+            rx.box(
+                rx.box(
+                    rx.text(
+                    "AI Generated Webpages",
+                    class_name="text-black font-bold text-3xl"
+                ),
+                rx.text(
+                    "Using AI to create websites offers speed and flexibility to any new design project. Build an AI generated website in real-time by writing a detailed prompt for the content you want to center the site on.",
+                    class_name="text-[#33373A99]"
+                ),
+                class_name="flex flex-col gap-1.5"
+                ),
+                rx.image(
+                        src="/screen.png",
+                        class_name="mx-auto w-64 h-40"
+                    ),
+                class_name="p-6 flex gap-4 flex-col rounded-lg bg-white max-w-sm w-full"
+            ),
+            rx.box(
+                rx.box(
+                    rx.text(
+                    "AI Generated Webpages",
+                    class_name="text-black font-bold text-3xl"
+                ),
+                rx.text(
+                    "Using AI to create websites offers speed and flexibility to any new design project. Build an AI generated website in real-time by writing a detailed prompt for the content you want to center the site on.",
+                    class_name="text-[#33373A99]"
+                ),
+                class_name="flex flex-col gap-1.5"
+                ),
+                rx.image(
+                        src="/screen.png",
+                        class_name="mx-auto w-64 h-40"
+                    ),
+                class_name="p-6 flex gap-4 flex-col rounded-lg bg-white max-w-sm w-full"
+            ),
             class_name="flex w-full justify-between"
         ),
         class_name="flex flex-col items-center mx-auto max-w-7xl w-full gap-12"
