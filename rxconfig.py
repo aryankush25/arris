ff913aa8102c5a2b609086d70d834f36a4b759b2
Aryan Agarwal <aryankush025@gmail.com>
2024-04-13 15:57:21 +0530

    Refactor database URL configuration in rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -1,6 +1,12 @@
+import os
 import reflex as rx
+
+database_url = os.environ.get(
+    "DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/arris"
+)
+
 
 config = rx.Config(
     app_name="arris",
-    db_url="postgresql://postgres:postgres@localhost:5432/arris",
+    db_url=database_url,
 )
