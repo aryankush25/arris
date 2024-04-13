54dd48308957cac322443f32989d2e197c905e5e
Ishita <ishita@gluelabs.com>
2024-04-13 15:32:29 +0530

    made a test api

--- arris/arris.py
+++ arris/arris.py
@@ -9,6 +9,7 @@ from arris.styles import BACKGROUND_COLOR, FONT_FAMILY, THEME, STYLESHEETS
 from arris.pages.tools import tools
 from arris.pages.team import team
 from arris.pages.index import index
+from arris.pages.myapi import api_test
 
 # Create app instance and add index page.
 app = rx.App(
@@ -19,3 +20,4 @@ app = rx.App(
 app.add_page(index, route="/")
 app.add_page(tools, route="/tools")
 app.add_page(team, route="/team")
+app.api.add_api_route("/items/{item_id}", api_test)
---
+++ arris/pages/myapi.py
@@ -0,0 +1,2 @@
+async def api_test(item_id: int):
+    return {"my_result": item_id}
