c6743d7a19e846dfb358426dbd44082b94417f52
Ishita <ishita@gluelabs.com>
2024-04-13 19:37:55 +0530

    features section layout

--- arris/pages/index.py
+++ arris/pages/index.py
@@ -61,7 +61,8 @@ import reflex as rx
 
 def index() -> rx.Component:
     return rx.box(
-        rx.box(rx.box(
+        #navbar
+            rx.box(
             rx.box(
                 "ARRIS",
                 class_name="font-semibold text-[#5B9BF3] text-5xl"
@@ -94,10 +95,49 @@ def index() -> rx.Component:
                 ),
                 class_name="flex gap-3 items-center"
             ),
-            class_name="flex justify-between w-full mt-14"
+            class_name="flex justify-between w-full mt-14 max-w-7xl mx-auto"
         ),
+        #features
+        rx.box(
+            rx.box(
+                rx.box(
+                rx.text(
+                    "Features",
+                    class_name="text-white"
+                ),
+                rx.box(
+                    "Strategic services drive digital success with tailored, comprehensive approaches.",
+                    class_name="text-white"
+                ),
+                class_name="flex flex-col border items-center gap-5"
+            ),
+            #three box
+            rx.box(
+                rx.box(
+                    rx.text(
+                        "AI Generated Webpages",
+                        class_name="text-black font-bold text-3xl"
+                    ),
+                    rx.text(
+                        "Using AI to create websites offers speed and flexibility to any new design project. Build an AI generated website in real-time by writing a detailed prompt for the content you want to center the site on.",
+                        class_name="text-[#33373A99]"
+                    ),
+                    rx.box(),
+                    class_name="p-6 flex flex-col rounded-lg bg-white max-w-sm w-full"
+                ),
+                rx.box(
+                    
+                ),
+                rx.box(
+
+                ),
+                class_name="flex w-full justify-between"
+            ),
+                class_name="flex flex-col items-center mx-auto max-w-7xl w-full gap-12"
+            ),
+            class_name="bg-[#5B9BF3] w-full mx-auto"
         ),
-        class_name="h-screen w-full flex flex-col mx-auto max-w-7xl from-blue-100 bg-opacity-30 via-white to-blue-100 bg-gradient-to-r"
+        class_name="min-h-screen w-full flex flex-col from-blue-100 bg-opacity-30 via-white to-blue-100 bg-gradient-to-r"
     )
 
 #     return rx.box(
