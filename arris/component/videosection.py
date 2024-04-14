b9e848effa830145cb8dbe7a37d781afb39d2711
Aryan Agarwal <aryankush025@gmail.com>
2024-04-15 02:23:33 +0530

    Fix video section formatting and responsiveness in videosection.py

--- arris/component/videosection.py
+++ arris/component/videosection.py
@@ -1,21 +1,24 @@
-import reflex as rx # type: ignore
+import reflex as rx  # type: ignore
+
 
 def videosection() -> rx.Component:
     return rx.box(
         rx.box(
             "Want to see our AI website builder in action?",
-            class_name="font-semibold text-2xl md:text-[32px] max-w-[630px]"
+            class_name="font-semibold text-2xl md:text-[32px] max-w-[630px]",
         ),
         rx.box(
-            rx.video(
-            url="https://www.youtube.com/watch?v=crDWEskuPII",
-            width="100%",
-            height="690px",
-            class_name="rounded-3xl",
-            playing=True,
-            loop=True
+            rx.box(
+                rx.video(
+                    url="https://www.youtube.com/watch?v=crDWEskuPII",
+                    width="100%",
+                    height="100%",
+                    playing=True,
+                    loop=True,
+                ),
+                class_name="rounded-lg overflow-hidden h-[300px] md:h-[690px]",
+            ),
+            class_name="p-11 bg-[#6FB2FF] rounded-lg w-full",
         ),
-            class_name="p-11 bg-[#6FB2FF] rounded-lg w-full"
-        ),
-        class_name="max-w-7xl justify-center items-center text-center px-4 mt-16 md:mt-28 mx-auto w-full flex flex-col gap-5"
+        class_name="max-w-7xl justify-center items-center text-center px-4 mt-16 md:mt-28 mx-auto w-full flex flex-col gap-5",
     )
