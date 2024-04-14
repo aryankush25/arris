5e66e5c6175f0873bedab92d3df654628c60cf58
Aryan Agarwal <aryankush025@gmail.com>
2024-04-15 02:29:41 +0530

    Fix formatting and import statement in navbar.py

--- arris/component/herosection.py
+++ arris/component/herosection.py
@@ -1,72 +1,74 @@
 import reflex as rx
 
+
 def herosection() -> rx.Component:
     def redirect_to_signup():
         return rx.redirect("/register")
+
     return rx.box(
+        rx.box(
+            rx.box(
+                "Creating",
+                class_name="font-bold text-[rgba(21, 23, 26, 1)] text-5xl md:text-7xl",
+            ),
+            rx.box(
+                "Websites",
+                class_name="font-bold text-[rgba(21, 23, 26, 1)] text-5xl md:text-7xl",
+            ),
+            rx.box(
+                "without Code",
+                rx.image(
+                    src="/line.png",
+                    alt="Descriptive text about the image",
+                    width="70%",
+                    class_name="mx-auto md:mx-0",
+                ),
+                class_name="flex flex-col gap-1 font-bold text-[rgba(21, 23, 26, 1)] text-5xl md:text-7xl",
+            ),
+            rx.box(
+                "Hassle",
+                class_name="font-bold text-[rgba(21, 23, 26, 1)] text-5xl md:text-7xl",
+            ),
             rx.box(
                 rx.box(
-                    "Creating",
-                    class_name="font-bold text-[rgba(21, 23, 26, 1)] text-5xl md:text-7xl"
-                ),
-                rx.box(
-                    "Websites",
-                    class_name="font-bold text-[rgba(21, 23, 26, 1)] text-5xl md:text-7xl"
-                ),
-                rx.box(
-                    "without Code",
-                    rx.image(
-                        src="/line.png",
-                        alt="Descriptive text about the image",
-                        width="70%",
-                        class_name="mx-auto md:mx-0"
-                    ),
-                    class_name="flex flex-col gap-1 font-bold text-[rgba(21, 23, 26, 1)] text-5xl md:text-7xl"
-                ),
-                rx.box(
-                    "Hassle",
-                    class_name="font-bold text-[rgba(21, 23, 26, 1)] text-5xl md:text-7xl"
-                ),
-                rx.box(
-                    rx.box(
                     "Explore a new AI-powered approach to",
-                    class_name="font-medium text-[rgba(21, 23, 26, 1)] text-xl md:text-2xl"
+                    class_name="font-medium text-[rgba(21, 23, 26, 1)] text-xl md:text-2xl",
                 ),
                 rx.box(
                     "website and UI element design.",
-                    class_name="font-medium text-[rgba(21, 23, 26, 1)] text-xl md:text-2xl"
+                    class_name="font-medium text-[rgba(21, 23, 26, 1)] text-xl md:text-2xl",
                 ),
-                class_name="flex flex-col"
-                ),
-                rx.box(
-                    rx.box(
-                        "Get Started",
-                        class_name="border cursor-pointer border-black rounded-lg text-xl items-center justify-center text-white bg-black px-6 py-3 md:py-4",
-                        on_click=redirect_to_signup
-                    ),
-                    rx.box(
-                        rx.image(
-                             src="/play.png",
-                             alt="Descriptive text about the image",
-                             box_size="14px",
-                             ),
-                             rx.box(
-                                  "View Demo",
-                                  class_name="text-lg font-medium text-[rgba(21, 23, 26, 1)]"
-                                  ),
-                        class_name="flex gap-1 items-center"
-                    ),
-                    class_name="flex gap-4 mt-6 justify-center md:justify-start"
-                ),
-                class_name="flex flex-col gap-2"
+                class_name="flex flex-col",
             ),
             rx.box(
-                rx.image(
-                    src="/cuate.png",
-                    alt="Descriptive text about the image",
-                    class_name="md:max-w-lg max-w-xl w-full"
+                rx.box(
+                    "Get Started",
+                    class_name="border cursor-pointer border-black rounded-lg text-xl items-center justify-center text-white bg-black px-6 py-3 md:py-4",
+                    on_click=redirect_to_signup,
                 ),
-                class_name="flex w-[50%] mt-4 justify-end h-[370px] max-h-[560px] h-full"
+                rx.box(
+                    rx.image(
+                        src="/play.png",
+                        alt="Descriptive text about the image",
+                        box_size="14px",
+                    ),
+                    rx.box(
+                        "View Demo",
+                        class_name="text-lg font-medium text-[rgba(21, 23, 26, 1)]",
+                    ),
+                    class_name="flex gap-1 items-center",
+                ),
+                class_name="flex gap-4 mt-6 justify-center md:justify-start",
             ),
-            class_name="flex flex-col md:flex-row px-4 gap-20 md:gap-0 justify-between w-full mt-16 text-center md:text-left items-center md:mt-28 max-w-7xl mx-auto"
-        )
+            class_name="flex flex-col gap-2",
+        ),
+        rx.box(
+            rx.image(
+                src="/cuate.png",
+                alt="Descriptive text about the image",
+                class_name="md:max-w-lg max-w-xl w-full",
+            ),
+            class_name="flex w-[50%] mt-4 justify-end h-[370px] max-h-[560px] h-full",
+        ),
+        class_name="flex flex-col md:flex-row px-4 gap-20 md:gap-0 justify-between w-full mt-16 text-center md:text-left items-center md:mt-28 max-w-7xl mx-auto",
+    )
--- arris/component/navbar.py
+++ arris/component/navbar.py
@@ -1,47 +1,47 @@
-import reflex as rx # type: ignore
+import reflex as rx  # type: ignore
 from arris.utils import ClientStorageState
+
 
 def navbar() -> rx.Component:
     def redirect_to_signup():
         return rx.redirect("/register")
+
     def redirect_to_signin():
         return rx.redirect("/login")
+
     def redirect_to_home():
         return rx.redirect("/home")
-    display_login: str = rx.Cookie("access-token")
+
     return rx.box(
         rx.box(
-            rx.image(
-                src="/company_logo.png"
-            ),
+            rx.image(src="/company_logo.png"),
             rx.text(
                 "ARRIS",
                 font_family="Integral CF",
-                class_name="font-semibold text-black text-[32px] md:text-5xl"
-
+                class_name="font-semibold text-black text-[32px] md:text-5xl",
             ),
-            class_name="flex items-center gap-2"
+            class_name="flex items-center gap-2",
         ),
         rx.cond(
-                ClientStorageState.custom_cookie,
-                rx.box(
-            "Go to Home",
-            class_name="text-white cursor-pointer md:text-lg py-1.5 flex items-center px-4 bg-black rounded-md",
-            on_click=redirect_to_home
-        ),
-                rx.box(
+            ClientStorageState.custom_cookie,
             rx.box(
-                "Login",
-                class_name="text-gray-500 md:text-lg font-medium cursor-pointer",
-                on_click=redirect_to_signin
+                "Go to Home",
+                class_name="text-white cursor-pointer md:text-lg py-1.5 flex items-center px-4 bg-black rounded-md",
+                on_click=redirect_to_home,
             ),
             rx.box(
-                "Sign Up",
-                class_name="text-white cursor-pointer md:text-lg py-2 px-[14px] bg-black rounded-md",
-                on_click=redirect_to_signup
+                rx.box(
+                    "Login",
+                    class_name="text-gray-500 md:text-lg font-medium cursor-pointer",
+                    on_click=redirect_to_signin,
+                ),
+                rx.box(
+                    "Sign Up",
+                    class_name="text-white cursor-pointer md:text-lg py-2 px-[14px] bg-black rounded-md",
+                    on_click=redirect_to_signup,
+                ),
+                class_name="flex gap-6 items-center",
             ),
-            class_name="flex gap-5 items-center"
         ),
-            ),
-        class_name="flex justify-between w-full md:mt-14 mt-8 px-4 max-w-7xl mx-auto"
+        class_name="flex justify-between w-full md:mt-8 mt-4 px-4 max-w-7xl mx-auto",
     )
