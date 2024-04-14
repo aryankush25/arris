4bd52e1f8df2b8069b933cb597dd49feec7a9883
Ishita <ishita@gluelabs.com>
2024-04-14 21:32:29 +0530

    added fallback pages

--- arris/arris.py
+++ arris/arris.py
@@ -12,6 +12,8 @@ from arris.services.shopify import shopifyOAuthCallback
 from arris.pages.login import login
 from arris.pages.home import home
 from arris.pages.builder import builder
+from arris.pages.pagenotfound import pagenotfound
+from arris.pages.noaccess import noaccess
 
 
 # Create app instance and add index page.
@@ -23,6 +25,8 @@ app = rx.App(
 app.add_page(index, route="/")
 app.add_page(login, route="/login")
 app.add_page(register, route="/register")
+app.add_page(pagenotfound, route="/pagenotfound")
 app.add_page(home, route="/home")
+app.add_page(noaccess, route="/noaccess")
 app.add_page(builder, route="/builder/[store_name]")
 app.api.add_api_route("/oauth/callback/", shopifyOAuthCallback)
---
+++ arris/pages/noaccess.py
@@ -0,0 +1,24 @@
+import reflex as rx # type: ignore
+
+def noaccess() -> rx.Component:
+    def redirect_to_signin():
+        return rx.redirect("/login")
+    return rx.box(
+        rx.image(
+            src="/noaccess.svg"
+        ),
+        rx.box(
+            "You don't have access to this page.",
+            class_name="mt-6 font-semibold text text-2xl max-w-[460px] text-center"
+        ),
+        rx.box(
+            "Please Log In",
+            class_name="mt-1 text-[#4F4F4F]"
+        ),
+        rx.box(
+            "Login",
+            class_name="py-3 px-6 bg-black font-semibold text-white rounded cursor-pointer mt-6",
+            on_click=redirect_to_signin
+        ),
+        class_name="h-screen w-full flex flex-col justify-center items-center from-blue-100 via-white to-blue-100 bg-gradient-to-r"
+    )
